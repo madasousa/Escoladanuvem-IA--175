a03c63c4cfef1846347b1631557b017395772465
Mada Almeida <168130616+madasousa@users.noreply.github.com>
2025-07-22 00:47:27 -0300

    Pratica 1
    Atividades python

---
+++ calculadora de preço total.py
@@ -0,0 +1,12 @@
+nome_do_produto = "cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade 
+
+print("Produto:", nome_do_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("---")
+print("Preço total: R$", preco_total)
+print("Obrigado por usar a calculadora de preço total!")
